6d416f9958a6e8d593c12b800c3ca24c939da6a1
Jacek <trojanowski.jacek@gmail.com>
2020-07-09 15:41:11 +0200

    hide secret key

--- link_app/settings.py
+++ link_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ku4eebz5fw&t8kq(95k$k48#6e6&*#u*kpel-e7_--9cutew(='
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
